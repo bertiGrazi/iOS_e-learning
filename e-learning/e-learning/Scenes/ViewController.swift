d290f6eabb7b174975ec881e689175bfb7c98064
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-09-19 10:57:08 -0300

    feat: delete storyboard and folders reorganize

---
+++ e-learning/e-learning/Scenes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  e-learning
+//
+//  Created by Grazi  Berti on 19/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
